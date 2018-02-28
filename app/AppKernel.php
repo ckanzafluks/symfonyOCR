b15c85c43fe689ef9316c902f8f427bab2a62acf
Camille <ckanza@fluksaqua.com>
2018-02-28 09:20:10 +0100

    installation bundle doctrine via composer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new OC\PlatformBundle\OCPlatformBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
