8ec6c622acaf871e53c88b0b8c2e7a59cd7cf122
Camille <ckanza@fluksaqua.com>
2018-03-09 13:18:10 +0100

    ajout flash messages

--- src/CustomCoreBundle/Controller/AdvertController.php
+++ src/CustomCoreBundle/Controller/AdvertController.php
@@ -1,7 +1,5 @@
 <?php
 
-// src/OC/PlatformBundle/Controller/AdvertController.php
-
 namespace CustomCoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -16,56 +14,27 @@ class AdvertController extends Controller
 
 	public function indexAction($page)
   	{
-	    
-		// Notre liste d'annonce en dur
-		$listAdverts = array(
-			array(
-				'title'   => 'Recherche développpeur Symfony',
-				'id'      => 1,
-				'author'  => 'Alexandre',
-				'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-				'date'    => new \Datetime()
-			),
-			array(
-				'title'   => 'Mission de webmaster',
-				'id'      => 2,
-				'author'  => 'Hugo',
-				'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-				'date'    => new \Datetime()
-			),
-			array(
-				'title'   => 'Offre de stage webdesigner',
-				'id'      => 3,
-				'author'  => 'Mathieu',
-				'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-				'date'    => new \Datetime()
-			)
-		);
-
-		// Et modifiez le 2nd argument pour injecter notre liste
-		return $this->render('CustomCoreBundle:Advert:index.html.twig', array(
+	    return $this->render('CustomCoreBundle:Advert:index.html.twig', array(
 			'listAdverts' => $listAdverts
 		));
   	}
 
 	public function viewAction($id)
 	{
-		$url = $this->get('router')->generate('oc_platform_home');
+		$url = $this->get('router')->generate('custome_core_home');
 		return new RedirectResponse($url);
 	}
 
 	public function menuAction()
   	{
-	    // On fixe en dur une liste ici, bien entendu par la suite
-	    // on la récupérera depuis la BDD !
         $listAdverts = array(
             array('id' => 2, 'title' => 'Recherche développeur Symfony'),
             array('id' => 5, 'title' => 'Mission de webmaster'),
             array('id' => 9, 'title' => 'Offre de stage webdesigner')
-        );
-        
+        );        
 	    return $this->render('CustomCoreBundle:Advert:menu.html.twig', array(
-	       'listAdverts' => $listAdverts
+            'listAdverts' => $listAdverts
 	    ));
   	}
+  	
 }
--- src/CustomCoreBundle/Controller/DefaultController.php
+++ src/CustomCoreBundle/Controller/DefaultController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace CustomCoreBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
@@ -12,12 +13,35 @@ class DefaultController extends Controller
     
     
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="xx")
      */
     public function indexAction()
     {
-        //return $this->render('@OCPlatform/Default/index.html.twig'); 
         return $this->render('@CustomCore/Default/index.html.twig'); 
+    }
+    
+    private function _initMenus() {
+        
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+        
+        $this->render('CustomCoreBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+    }
+    
+    
+    public function contactAction(Request $request) {
+        $session = $request->getSession();
+        $session->getFlashBag()->add('info', 'La page de contact n\'est pas encore disponible. Merci de revenir plus tard!');
+        
+        return $this->redirectToRoute('custom_core_homepage');
+        
+        ///$url = $this->get('router')->generate('custom_core_homepage');
+        //return new RedirectResponse($url);
     }
     
     
